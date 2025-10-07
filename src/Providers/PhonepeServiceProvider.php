8a246f233a112af5aea96a06eb08b6d5fc37797e
Saju G <saju.g@wontonetech.com>
2025-10-07 11:39:48 +0530

    some composer updation

--- src/Providers/PhonepeServiceProvider.php
+++ src/Providers/PhonepeServiceProvider.php
@@ -37,8 +37,37 @@ class PhonepeServiceProvider extends ServiceProvider
 
         // Publish configuration files
         $this->publishes([
+            __DIR__ . '/../Config/paymentmethods.php' => config_path('paymentmethods.php'),
+        ], 'phonepe-config');
+
+        // Publish system configuration
+        $this->publishes([
+            __DIR__ . '/../Config/system.php' => config_path('phonepe-system.php'),
+        ], 'phonepe-config');
+
+        // Publish views
+        $this->publishes([
+            __DIR__ . '/../Resources/views' => resource_path('views/vendor/phonepe'),
+        ], 'phonepe-views');
+
+        // Publish translations
+        $this->publishes([
+            __DIR__ . '/../Resources/lang' => lang_path('vendor/phonepe'),
+        ], 'phonepe-lang');
+
+        // Publish assets
+        $this->publishes([
             __DIR__ . '/../Resources/assets' => public_path('vendor/wontonee/phonepe'),
         ], 'phonepe-assets');
+
+        // Publish everything
+        $this->publishes([
+            __DIR__ . '/../Config/paymentmethods.php' => config_path('paymentmethods.php'),
+            __DIR__ . '/../Config/system.php' => config_path('phonepe-system.php'),
+            __DIR__ . '/../Resources/views' => resource_path('views/vendor/phonepe'),
+            __DIR__ . '/../Resources/lang' => lang_path('vendor/phonepe'),
+            __DIR__ . '/../Resources/assets' => public_path('vendor/wontonee/phonepe'),
+        ], 'phonepe');
     }
 
     /**
